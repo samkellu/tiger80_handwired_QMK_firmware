2cdf67ee2691becd18d97caf9ef555be5a5419e7
sam <samkelly1290@gmail.com>
2023-07-04 20:28:56 +1000

    keyboard layout finalised and implemented

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,6 +3,21 @@
 
 #include QMK_KEYBOARD_H
 
+#define LAYOUT(K0, K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, \
+			   K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K30, K31, \
+			   K32, K33, K34, K35, K36, K37, K38, K39, K40, K41, K42, K43, K44, K45, K46, \
+			   K47, K48, K49, K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, \
+			   K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K70, K71, K72, \
+			   K73, K74, K75, K76, K77, K78, K79, K80, K81, K82, K83) \
+			{ \
+				{K0,  K1,  K2,  K3,  K4,  K5,  K6,  K7,  K8,  K9,  K10, K11, K12, K13, K14, K15, K16}, \
+				{K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K30, K31}, \
+				{K32, K33, K34, K35, K36, K37, K38, K39, K40, K41, K42, K43, K44, K45, K46}, \
+				{K47, K48, K49, K50, K51, K52, K53, K54, K55, K56, K57, K58, KC_NO, K59}, \
+				{K60, KC_NO, K61, K62, K63, K64, K65, K66, K67, K68, K69, K70, KC_NO, K71, KC_NO, K72}, \
+				{K73, K74, K75, KC_NO, KC_NO, KC_NO, K76, KC_NO, KC_NO, KC_NO, K77, K78, K79, K80, K81, K82, K83} \
+			}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
@@ -20,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│Meta│Alt │                        │ Alt│Meta│Menu│Ctrl│ │ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
      */
-    [0] = LAYOUT_tkl_ansi(
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
+    [0] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_DEL, KC_PSCR, KC_HOME,
 
-        KC_GRV,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_1,    KC_BSPC,    KC_PGUP, KC_NO,   KC_NO, 
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_PGDN, KC_NO,   KC_NO,
+        KC_GRV,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_1,    KC_BSPC,    KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_PGDN,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
