a0bb0c2b2139abc116d698642aa079e29dc69c3a
sam <skel4720@uni.sydney.edu.au>
2024-07-17 18:54:49 +1000

    proto map gen is now functional

--- map_gen.py
+++ map_gen.py
@@ -1,23 +1,26 @@
 import random
 
 MIN_ROOM_WIDTH = 2
-MAX_REC_DEPTH = 8
+MAX_REC_DEPTH = 4
 
 def bsp(map, l, r, t, b, depth):
     if depth == 0:
         if (r-MIN_ROOM_WIDTH <= l+MIN_ROOM_WIDTH or b-MIN_ROOM_WIDTH <= t+MIN_ROOM_WIDTH):
             return map
 
-        split_l = l+1#random.randrange(l+MIN_ROOM_WIDTH, r-MIN_ROOM_WIDTH)
-        split_r = r-1#random.randrange(split_l, r-MIN_ROOM_WIDTH)
-        split_t = t+1#random.randrange(t+MIN_ROOM_WIDTH, b-MIN_ROOM_WIDTH)
-        split_b = b-1#random.randrange(split_t, b-MIN_ROOM_WIDTH)
+        split_l = l + MIN_ROOM_WIDTH
+        split_r = r - MIN_ROOM_WIDTH
+        split_t = t + MIN_ROOM_WIDTH
+        split_b = b - MIN_ROOM_WIDTH
 
         print(f"{l} {r} {t} {b}")
         for j in range(split_t, split_b):
             for i in range(split_l, split_r):
                 map[i][j] = 0
-
+        print(f"({split_l}, {split_b}) -- ({split_l}, {split_t})")
+        print(f"({split_l}, {split_t}) -- ({split_r}, {split_t})")
+        print(f"({split_r}, {split_t}) -- ({split_r}, {split_b})")
+        print(f"({split_r}, {split_b}) -- ({split_l}, {split_b})")
         return map
 
     if (r - l >= b - t):
@@ -58,9 +61,7 @@ def bsp(map, l, r, t, b, depth):
 
 if __name__ == "__main__":
 
-    random.seed(64)
     map = [[1] * 40 for i in range(40)]
-
     map = bsp(map, 0, 40, 0, 40, MAX_REC_DEPTH)
     for i in map:
         print("".join(['1' if x == 1 else ' ' for x in i]))
